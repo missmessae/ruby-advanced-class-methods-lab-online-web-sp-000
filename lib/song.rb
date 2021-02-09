1deb9169d5d3ad7fe5478bd9290100d202e9b754
Annie Wiegel <annewiegel17@gmail.com>
2021-02-09 22:32:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,9 +34,11 @@ class Song
   end
 
   def self.find_or_create_by_name(title)
-    if self.find_by_name(title) == false
-      self.create_by_name(title)
+    result = self.find_by_name(title)
+    if result
+      result
     else
+      self.create_by_name(title)
     end
   end
 
