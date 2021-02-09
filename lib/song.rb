f9756ec9e3007667375a1044da405b65644cdb46
Annie Wiegel <annewiegel17@gmail.com>
2021-02-09 22:21:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,9 +17,9 @@ class Song
       song
   end
 
-  def self.new_by_name(name)
-      name = self.new
-      song.name = name
+  def self.new_by_name(title)
+      song = self.new
+      song.name = title
       song
   end
 
