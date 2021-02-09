4b0cadb80b02922a42f37ba4c923ae6279de2fda
Annie Wiegel <annewiegel17@gmail.com>
2021-02-09 22:33:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,5 +47,17 @@ class Song
    sorted
  end
 
+ def self.create_from_filename(filename)
+  result = self.new_from_filename(filename)
+  song = self.create
+  song.name = result.name
+  song.artist_name = result.artist_name
+  song
+end
+
+def self.destroy_all
+  self.all.clear
+end
+
 
 end
